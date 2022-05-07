e49af53daf7986fc7f9705a6b2b4cbb3a114cde2
Bogdan Langhe <bogdanlanghe@gmail.com>
2022-05-07 22:38:41 +0300

    add new travel agent date

--- main.js
+++ main.js
@@ -3,16 +3,22 @@ alert("Work in progress");
 
 const juniorWebDev = document.querySelector("#junior-web-developer");
 const webContentManager = document.querySelector("#web-content-manager");
+const travelAgent = document.querySelector("#travel-agent");
 const juniorWebDevBegin = 2020;
 const webContentManagerBegin = juniorWebDevBegin;
+const otherJobs = {
+  travelAgent: 2019,
+  concierge: 2008,
+}
 let current = '';
 //get current year
 const year = new Date().getFullYear();
-if (year){
-  current = "(Current job)"
+if (year > juniorWebDevBegin) {
+  current = `(Current job / Experience: ${year - juniorWebDevBegin} years)`;
 }
 juniorWebDev.textContent = juniorWebDevBegin.toString() + " - " + year.toString() + " " + current;
 webContentManager.textContent = webContentManagerBegin.toString() + " - " + year.toString() + " " + current;
+travelAgent.textContent = otherJobs.travelAgent.toString() + " - " + juniorWebDevBegin.toString();
 
 
 
