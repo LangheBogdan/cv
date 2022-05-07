0a4f3436fe1aa1574277b4d29e595c072623afbe
Bogdan Langhe <bogdanlanghe@gmail.com>
2022-05-07 19:25:49 +0300

    change current job end date dynamic with js method getFullYear();

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 // alert on document ready
 alert("Work in progress");
+
+const juniorWebDev = document.querySelector("#junior-web-developer");
+const webContentManager = document.querySelector("#web-content-manager");
+const juniorWebDevBegin = 2020;
+const webContentManagerBegin = juniorWebDevBegin;
+let current = '';
+//get current year
+const year = new Date().getFullYear();
+if (year){
+  current = "(Current job)"
+}
+juniorWebDev.textContent = juniorWebDevBegin.toString() + " - " + year.toString() + " " + current;
+webContentManager.textContent = webContentManagerBegin.toString() + " - " + year.toString() + " " + current;
+
+
+
